ba9511639a9674377e5fa0c1779bf135c3196d05
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-06-10 13:38:00 +0200

    Added TDNN
    We are getting to the point where I don't know what I'm doing

--- tdnn_practice.py
+++ tdnn_practice.py
@@ -4,11 +4,22 @@ import os
 import glob
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from scipy.io import wavfile
 
 # Push calculation to gpu if possible
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Set important variables
+batch_size = 1
+input_size = 24
+hidden_size = 512
+frames = 1 #TODO ?
+num_classes = 10
+learning_rate = 0.01
+num_epochs = 2
 
 # LOAD DATA
 
@@ -26,6 +37,9 @@ class Dataset(Dataset):
     # Can be called as dataset[i]
     def __getitem__(self, index):
         return torch.from_numpy(self.samples[index]), self.classes[index]
+
+    def __len__(self):
+        return self.n_samples
     
     # Load the training data and save all relevant info in arrays
     # TODO PREPROCESS DATA BEFORE THIS
@@ -85,12 +99,89 @@ class Dataset(Dataset):
         self.sampling_rates = rates
         self.n_samples = len(self.samples)
 
+# Make two datasets for training and testing with different samples
 train_dataset = Dataset()
-test_dataset = Dataset()
-
 train_dataset.load_train_data()
+test_dataset = Dataset()
 test_dataset.load_test_data()
 
-for train, test in zip(train_dataset, test_dataset):
-    print("train: ",  train)
-    print("test: ",  test)
+# Set up dataloader for easy access to shuffled data batches
+train_data_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_data_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# Maybe visualize select data samples
+
+
+
+# DEFINE NEURAL NETWORK
+
+class TDNN(nn.Module):
+    def __init__(self, input_size, hidden_size, frames, num_classes):
+        super(TDNN, self).__init__()
+
+        self.time_context_layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size), #TODO add time context somehow
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1500),
+            nn.ReLU()
+        )
+
+        self.pooling_layer = nn.Sequential(
+            nn.MaxPool2d(1500 * frames, 3000),
+            #nn.ReLU()
+        )
+        
+        self.segment_layers = nn.Sequential(
+            nn.Linear(3000, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_classes),
+            nn.ReLU()
+        )
+
+        self.softmax = nn.Sequential( #TODO use softmax? not if i use nn.CrossEntropyLoss()
+            nn.Softmax(), #TODO how to use softmax?
+            #nn.ReLU()
+        )
+        
+    def forward(self, x):
+        x = self.time_context_layers(x)
+        x = self.pooling_layer(x)
+        x = self.segment_layers(x)
+        #x = self.softmax(x) #TODO use softmax? not if i use nn.CrossEntropyLoss()
+        return x #TODO maybe also return seg6 and seg7 for the x-vector?
+
+model = TDNN(input_size, hidden_size, frames, num_classes).to(device)
+# Alternatively load an existing model dictionary
+#model.load_state_dict(torch.load(PATH))
+#model.eval()
+
+# Set up loss and optimizer
+criterion = nn.CrossEntropyLoss() #also applies softmax
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop
+n_total_steps = len(train_data_loader)
+
+for epoch in range(num_epochs):
+    for i, (sapmles, labels) in enumerate(train_data_loader):
+        # Reshape sample data
+        #sapmles = sapmles.reshape(-1, 120).to(device)
+        #labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(sapmles)
+        loss = criterion(outputs, labels)
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print(f'epoch {epoch+1} / {num_epochs}, step {i+1} / {n_total_steps}, loss = {loss.item():.4f}')
