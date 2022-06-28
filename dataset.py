ce28bf88a788cb80833111e0e477df35e1867492
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-06-28 13:14:35 +0200

    switched to VoxCeleb dataset

--- dataset.py
+++ dataset.py
@@ -13,7 +13,7 @@ from torch.utils.data import Dataset
 class Dataset(Dataset): 
     def __init__(self):
         self.classes = 0
-        self.all_classes = 0
+        self.unique_classes = 0
 
         self.samples = 0
         self.sampling_rates = 0
@@ -22,89 +22,57 @@ class Dataset(Dataset):
     # Returns the sample and class at the given index
     # Can be called as dataset[i] and works with dataloader
     def __getitem__(self, index):
-        return torch.from_numpy(self.samples[index]), self.all_classes.index(self.classes[index])
+        return torch.from_numpy(self.samples[index]), self.unique_classes.index(self.classes[index])
 
     def __len__(self):
         return self.n_samples
     
     # Load the training data and save all relevant info in arrays
     # TODO Preprocess data before this if neccessary
-    def load_train_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=1103):
+    def load_train_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=512):
         # Get the paths to all the data samples
-        globs = glob.glob('data\wavfiles\*\*.wav')
-
-        # Get only every second sample for the training data samples
-        # TODO put training and testing data in different folders to make this step easier
-        for i, g in enumerate(globs):
-            if(i%(2) == 0):
-                globs[int(i/(2))] = globs[i]
-        globs = globs[:int(len(globs)/(2))]
+        globs = glob.glob('../VoxCelebDownload/data/VoxCeleb/vox1_dev_wav/id1000*/*/*.wav') #TODO replace id100* with * to load all samples
 
         # Get the class names from the paths
-        self.classes = np.array([os.path.basename(os.path.dirname(f)) for f in globs])
-        self.all_classes = list(np.unique(self.classes))
+        self.classes = np.array([os.path.basename(os.path.dirname(os.path.dirname(f))) for f in globs])
+        self.unique_classes = list(np.unique(self.classes))
 
         # Gat the list of samples, classes and the sampling rate
         samples = []
         rates = []
-        classes = []
-        for i, g in enumerate(globs):
-            rate, sample = wavfile.read(g, np.dtype)
-            clas = self.classes[i]
-            sub_samle_length = int(rate)
+        for g in globs:
             print("load train sample: ", g)
-            # Split the sample into several 0.1 second long samples to increase number of samples
-            # Because the mel frequencies look very different for all 10 instruments cutting down sample length like this should still give good results
-            # TODO this takes too long to do every time so do this in preprocessing ahead of time (might not be neccessary with proper dataset)
-            for i in range(math.floor(len(sample)/sub_samle_length)-1):
-                small_sample = sample[i*sub_samle_length : i*sub_samle_length + sub_samle_length]
-                small_sample = mfcc(small_sample, rate, numcep=mfcc_numcep, nfilt=mfcc_nfilt, nfft=mfcc_nfft)
-                samples.append(np.array(small_sample))
-                rates.append(rate)
-                classes.append(clas)
+            rate, sample = wavfile.read(g, np.dtype)
+            sample = mfcc(sample, rate, numcep=mfcc_numcep, nfilt=mfcc_nfilt, nfft=mfcc_nfft)
+            samples.append(np.array(sample))
+            rates.append(rate)
+
+        #TODO add data augmentation to increase number of training samples
 
         self.samples = samples
         self.sampling_rates = rates
-        self.classes = classes
         self.n_samples = len(self.samples)
     
     # Load the testing data and save all relevant info in arrays
     # TODO Preprocess data before this if neccessary
-    def load_test_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=1103): #nfft should be 512 I think but that gives warning messages
+    def load_test_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=512): #nfft should be 512 I think but that gives warning messages
         # Get the paths to all the data samples
-        globs = glob.glob('data\wavfiles\*\*.wav')
-
-        # Get only every second sample for the testing data samples
-        # TODO put training and testing data in different folders to make this step easier
-        for i, g in enumerate(globs):
-            if(i%(2) == 1):
-                globs[int((i-1)/(2))] = globs[i]
-        globs = globs[:int(len(globs)/(2))]
+        globs = glob.glob('../VoxCelebDownload/data/VoxCeleb/vox1_test_wav/*/*/*.wav') #TODO replace id100* with * to load all samples
 
         # Get the class names from the paths
-        self.classes = np.array([os.path.basename(os.path.dirname(f)) for f in globs])
-        self.all_classes = list(np.unique(self.classes))
+        self.classes = np.array([os.path.basename(os.path.dirname(os.path.dirname(f))) for f in globs])
+        self.unique_classes = list(np.unique(self.classes))
 
         # Gat the list of samples, classes and the sampling rate
         samples = []
         rates = []
-        classes = []
-        for i, g in enumerate(globs):
+        for g in globs:
+            print("load train sample: ", g)
             rate, sample = wavfile.read(g, np.dtype)
-            clas = self.classes[i]
-            sub_samle_length = int(rate)
-            print("load test sample: ", g)
-            # Split the sample into several 0.1 second long samples to increase number of samples
-            # Because the mel frequencies look very different for all 10 instruments cutting down sample length like this should still give good results
-            # TODO this takes too long to do every time so do this in preprocessing ahead of time (might not be neccessary with proper dataset)
-            for i in range(math.floor(len(sample)/sub_samle_length)-1):
-                small_sample = sample[i*sub_samle_length : i*sub_samle_length + sub_samle_length]
-                small_sample = mfcc(small_sample, rate, numcep=mfcc_numcep, nfilt=mfcc_nfilt, nfft=mfcc_nfft)
-                samples.append(np.array(small_sample))
-                rates.append(rate)
-                classes.append(clas)
+            sample = mfcc(sample, rate, numcep=mfcc_numcep, nfilt=mfcc_nfilt, nfft=mfcc_nfft)
+            samples.append(np.array(sample))
+            rates.append(rate)
 
         self.samples = samples
         self.sampling_rates = rates
-        self.classes = classes
         self.n_samples = len(self.samples)
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from neural_net import NeuralNet
 
 class Config:
     def __init__(self, batch_size=100, input_size=24, hidden_size=512, num_classes=10, learning_rate=0.001,
-                num_epochs=5, path='./trained_models/x_vector_model.pth'):
+                num_epochs=3, path='./trained_models/x_vector_model.pth'):
         self.batch_size = batch_size
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -18,18 +18,23 @@ class Config:
         self.num_epochs = num_epochs
         self.path = path
 
-def load_data():
-    # Make two datasets for training and testing with different samples
+def load_train_data():
     train_dataset = Dataset()
     train_dataset.load_train_data()
+
+    # Set up dataloader for easy access to shuffled data batches
+    train_data_loader = DataLoader(dataset=train_dataset, batch_size=config.batch_size, shuffle=True, drop_last=True)
+
+    return train_data_loader
+
+def load_test_data():
     test_dataset = Dataset()
     test_dataset.load_test_data()
 
     # Set up dataloader for easy access to shuffled data batches
-    train_data_loader = DataLoader(dataset=train_dataset, batch_size=config.batch_size, shuffle=True, drop_last=True)
     test_data_loader = DataLoader(dataset=test_dataset, batch_size=config.batch_size, shuffle=False, drop_last=True)
 
-    return train_data_loader, test_data_loader
+    return test_data_loader
 
 def setup_model(load_existing=False):
     model = NeuralNet(config.input_size, config.hidden_size, config.num_classes)
@@ -46,7 +51,7 @@ def setup_model(load_existing=False):
 
     return model, criterion, optimizer
 
-def train():
+def train(train_data):
     total_steps = len(train_data)
     for epoch in range(config.num_epochs):
         model.train()
@@ -65,11 +70,11 @@ def train():
             loss.backward()
             optimizer.step()
 
-            if (i+1) % 1 == 0:
+            if (i+1) % 100 == 0:
                 print(f'epoch {epoch+1} / {config.num_epochs}, step {i+1} / {total_steps}, loss = {loss.item():.4f}')
             #TODO return extra data for the graphs in the thesis
 
-def test():
+def test(test_data):
     with torch.no_grad():
         n_correct = 0
         n_samples = 0
@@ -95,21 +100,22 @@ if __name__ == "__main__":
     print(torch.cuda.is_available())
 
     # Set important variables
-    config = Config()
+    config = Config(num_epochs=1)
 
     # Load data
-    train_data, test_data = load_data()
+    train_data = load_train_data()
+    # test_data = load_test_data()
 
     # TODO Maybe visualize select data samples for images for the thesis
 
     # Define neural network
-    model, criterion, optimizer = setup_model(load_existing=True)
+    model, criterion, optimizer = setup_model(load_existing=False)
 
     # Training loop
-    train()
+    train(train_data)
 
     # Save the model dictionary
     torch.save(model.state_dict(), config.path)
 
     # Testing
-    test()
+    # test(test_data)
