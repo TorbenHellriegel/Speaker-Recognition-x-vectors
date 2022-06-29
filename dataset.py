e638180da9bc6b804cc196237d118a71059838c6
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-06-29 18:02:52 +0200

    Refactor code for pytorch lightning

--- dataset.py
+++ dataset.py
@@ -9,9 +9,9 @@ from scipy.io import wavfile
 from torch.utils.data import Dataset
 
 train_path1 = '../../../../../../../../../data/7hellrie/VoxCeleb/vox1_dev_wav/id1000*/*/*.wav'
-test_path1 = '../../../../../../../../../data/7hellrie/VoxCeleb/vox1_test_wav/*/*/*.wav'
+test_path1 = '../../../../../../../../../data/7hellrie/VoxCeleb/vox1_test_wav/id103*/*/*.wav'
 train_path2 = 'data/VoxCeleb/vox1_dev_wav/wav/id1000*/*/00001.wav'
-test_path2 = 'data/VoxCeleb/vox1_test_wav/wav/*/*/*.wav'
+test_path2 = 'data/VoxCeleb/vox1_test_wav/wav/id103*/*/00001.wav'
 
 # TODO adjust based on what the dataset looks like
 class Dataset(Dataset): 
@@ -35,7 +35,7 @@ class Dataset(Dataset):
     # TODO Preprocess data before this if neccessary
     def load_train_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=512):
         # Get the paths to all the data samples
-        globs = glob.glob(train_path2) #TODO replace id100* with * to load all samples
+        globs = glob.glob(train_path1) #TODO replace id100* with * to load all samples
 
         # Get the class names from the paths
         self.classes = np.array([os.path.basename(os.path.dirname(os.path.dirname(f))) for f in globs])
@@ -71,7 +71,7 @@ class Dataset(Dataset):
     # TODO Preprocess data before this if neccessary
     def load_test_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=512): #nfft should be 512 I think but that gives warning messages
         # Get the paths to all the data samples
-        globs = glob.glob(test_path2)
+        globs = glob.glob(test_path1)
 
         # Get the class names from the paths
         self.classes = np.array([os.path.basename(os.path.dirname(os.path.dirname(f))) for f in globs])
@@ -82,7 +82,7 @@ class Dataset(Dataset):
         rates = []
         classes = []
         for i, g in enumerate(globs):
-            print("load train sample: ", g)
+            print("load test sample: ", g)
             rate, sample = wavfile.read(g, np.dtype)
             clas = self.classes[i]
             sub_sample_length = int(rate * 3)
