aafeee838a01bb1c3cbd3e97a2af5a7e0d2e762e
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-06-29 14:34:18 +0200

    Adjusted path for other computer

--- dataset.py
+++ dataset.py
@@ -8,6 +8,10 @@ from python_speech_features import mfcc
 from scipy.io import wavfile
 from torch.utils.data import Dataset
 
+train_path1 = '../../../../../../../../../data/7hellrie/VoxCeleb/vox1_dev_wav/id1000*/*/*.wav'
+test_path1 = '../../../../../../../../../data/7hellrie/VoxCeleb/vox1_test_wav/*/*/*.wav'
+train_path2 = 'data/VoxCeleb/vox1_dev_wav/wav/id1000*/*/00001.wav'
+test_path2 = 'data/VoxCeleb/vox1_test_wav/wav/*/*/*.wav'
 
 # TODO adjust based on what the dataset looks like
 class Dataset(Dataset): 
@@ -31,7 +35,7 @@ class Dataset(Dataset):
     # TODO Preprocess data before this if neccessary
     def load_train_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=512):
         # Get the paths to all the data samples
-        globs = glob.glob('../../../../../../../../../data/7hellrie/VoxCeleb/vox1_dev_wav/id1000*/*/*.wav') #TODO replace id100* with * to load all samples
+        globs = glob.glob(train_path2) #TODO replace id100* with * to load all samples
 
         # Get the class names from the paths
         self.classes = np.array([os.path.basename(os.path.dirname(os.path.dirname(f))) for f in globs])
@@ -67,7 +71,7 @@ class Dataset(Dataset):
     # TODO Preprocess data before this if neccessary
     def load_test_data(self, mfcc_numcep=24, mfcc_nfilt=26, mfcc_nfft=512): #nfft should be 512 I think but that gives warning messages
         # Get the paths to all the data samples
-        globs = glob.glob('../../../../../../../../../data/7hellrie/VoxCeleb/vox1_test_wav/*/*/*.wav')
+        globs = glob.glob(test_path2)
 
         # Get the class names from the paths
         self.classes = np.array([os.path.basename(os.path.dirname(os.path.dirname(f))) for f in globs])
--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def test(test_data):
 
 if __name__ == "__main__":
     # Push calculation to gpu if possible
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = 'cpu'#torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     print(torch.cuda.is_available())
 
