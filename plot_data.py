1d9a6b7948b5f27aa5e45ec8df8ff15a77b7a8d4
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-10-28 15:45:20 +0200

    more plots for thesis

--- plot_data.py
+++ plot_data.py
@@ -248,3 +248,19 @@ if(False):
     # ax.set_title('False Positive Scores')
 
     # plt.show()
+
+    Print('done')
+
+t = np.linspace(-10, 10, 100)
+sig = 1 / (1 + np.exp(-t))
+
+plt.axhline(y=0, color="black", linestyle="--")
+plt.axhline(y=0.5, color="black", linestyle=":")
+plt.axhline(y=1.0, color="black", linestyle="--")
+plt.axvline(color="grey")
+plt.axline((0, 0.5), slope=0.25, color="black", linestyle=(0, (5, 5)))
+plt.plot(t, sig, linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
+plt.xlim(-10, 10)
+plt.xlabel("t")
+plt.legend(fontsize=14)
+plt.savefig('sigmoid.pdf')
