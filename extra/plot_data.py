b77cef51cd069c09587f4029d0632efbd9656d35
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-10-30 13:03:18 +0100

    more data plots for thesis

--- extra/plot_data.py
+++ extra/plot_data.py
@@ -1,7 +1,7 @@
-from cProfile import label
-import pandas as pd
-import numpy as np
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 if(False):
     tag = np.array(['train_step_acc', 'train_step_loss', 'val_step_acc', 'val_step_loss'])
@@ -71,27 +71,27 @@ if(False):
     val_acc_y_full = np.concatenate(val_acc_y_full)
     val_loss_y_full = np.concatenate(val_loss_y_full)
 
+    plt.rcParams.update({'font.size': 34})
+
     plt.figure(figsize=(16,12), layout='constrained')
-    plt.rcParams['font.size'] = '28'
-    plt.plot(train_acc_x_full, train_acc_y_full, label='train_acc')
-    plt.plot(val_acc_x_full, val_acc_y_full, label='val_acc', linewidth=3)
-    plt.xlabel('steps')
+    plt.plot(train_acc_x_full/783, train_acc_y_full, label='train_acc')
+    plt.plot(val_acc_x_full/783, val_acc_y_full, label='val_acc', linewidth=3)
+    plt.xlabel('epoch')
     plt.ylabel('accuracy')
     plt.title("Training and Validation Accuracy")
     plt.grid(True)
-    plt.legend()
-    plt.savefig('train_val_acc.svg')
+    plt.legend(fontsize=38)
+    plt.savefig('train_val_acc.pdf', bbox_inches='tight')
 
     plt.figure(figsize=(16,12), layout='constrained')
-    plt.rcParams['font.size'] = '28'
-    plt.plot(train_loss_x_full, train_loss_y_full, label='train_loss')
-    plt.plot(val_loss_x_full, val_loss_y_full, label='val_loss', linewidth=3)
-    plt.xlabel('steps')
+    plt.plot(train_loss_x_full/783, train_loss_y_full, label='train_loss')
+    plt.plot(val_loss_x_full/783, val_loss_y_full, label='val_loss', linewidth=3)
+    plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.title("Training and Validation Loss")
     plt.grid(True)
-    plt.legend()
-    plt.savefig('train_val_loss.svg')
+    plt.legend(fontsize=38)
+    plt.savefig('train_val_loss.pdf', bbox_inches='tight')
 
 if(False):
     #Data
@@ -200,67 +200,116 @@ if(False):
 
     fig, ax = plt.subplots()
 
-    colors = np.where(pairs==1, 'r', 'c')
+    colors = np.where(pairs==1, 'red', 0.8)
+    colors = np.where(pairs==2, 'green', colors)
+    colors = np.where(pairs==3, 'orange', colors)
 
-    # y_pos = y_pos[:10]
-    # performance = performance[:10]
-    # colors = colors[:10]
+    red_patch = mpatches.Patch(color='red', label='286 + 303')
+    green_patch = mpatches.Patch(color='green', label='274 + 301')
+    blue_patch = mpatches.Patch(color='orange', label='285 + 289')
+    white_patch = mpatches.Patch(color='grey', label='other')
+    ax.legend(handles=[red_patch,green_patch,blue_patch,white_patch])
 
     ax.barh(y_pos, performance, color=colors)
     ax.invert_yaxis()
+    ax.get_yaxis().set_ticks([])
     ax.set_xlabel('LLR Score')
     ax.set_title('False Positive Scores')
 
+    plt.show()
+    #plt.savefig('False_Positive_Scores.pdf', bbox_inches='tight')
+
+    print('done')
+
+if(True):
+    labels = [(10286, 10303), (10274, 10301), (10285, 10289), (10279, 10290), (10300, 10301), (10285, 10288), (10302, 10304), (10286, 10307), (10270, 10288), (10270, 10308), (10303, 10307), (10297, 10298), (10280, 10282), (10276, 10305), (10297, 10305), (10298, 10305), (10276, 10290), (10293, 10304), (10281, 10282)]
+    num_pairs = [1, 1, 1, 2, 1, 1, 3, 1, 2, 3, 1, 2, 2, 1, 2, 3, 2, 4, 2, 5, 1, 1, 2, 2, 5, 2, 2, 3, 6, 1, 7, 3, 8, 2, 1, 1, 2, 1, 5, 1, 3, 2, 8, 1, 9, 1, 1, 3, 1, 8, 3, 10, 1, 1, 1, 1, 2, 1, 3, 1, 8, 1, 3, 1, 2, 11, 1, 3, 3, 3, 2, 1, 1, 12, 2, 5, 2, 8, 2, 1, 2, 2, 13, 14, 1, 1, 15, 14, 1, 16, 17, 8, 2, 2, 18, 14, 8, 19, 8, 11]
+    scores = [27.705821411993384, 27.06155240270146, 26.690404570158073, 26.231146952337706, 25.59046231986114, 25.27378797744494, 24.8576751162702, 24.79027244517839, 24.73023401071766, 24.663622717646078, 24.535161600485942, 24.422140889130464, 24.342783065046646, 24.21828079278385, 24.002277854864197, 23.96818361438727, 23.96233057030901, 23.954883035728823, 23.84017266295163, 23.660798188249274, 23.359508612408433, 23.312426081951003, 23.174175524203932, 22.761232500399352, 22.657207612631844, 22.6534289530776, 22.49677408028876, 22.445564078001908, 22.34360070491359, 22.194878920409334, 22.181158290046543, 22.15128047036191, 22.03093675914478, 21.941824456632524, 21.86719116325947, 21.817618245094458, 21.640004423565834, 21.621708864098153, 21.527026544623524, 21.49016777594293, 21.487020736334678, 21.484317949856667, 21.471152457337105, 21.396805794776, 21.245468190680022, 20.965597638893477, 20.910562573750248, 20.894739566950825, 20.87018132601225, 20.82643461458366, 20.788735206150285, 20.76114690686577, 20.698241491287572, 20.65961983608179, 20.618075793110158, 20.583410159116227, 20.58067519020448, 20.560699672982217, 20.5365368650693, 20.467099591458584, 20.423588418792527, 20.402707115089733, 20.361296727383337, 20.323646153525125, 20.290637614417932, 20.278865692080444, 20.22161909197351, 20.2025658277199, 20.143087377119684, 19.925186541350875, 19.87105594069614, 19.78780149043257, 19.730171593029638, 19.718436555512838, 19.707233487023263, 19.6727489121931, 19.635619016121765, 19.63168358951379, 19.569183228477122, 19.558240880798124, 19.522377232545104, 19.519172663904243, 19.49284576078555, 19.473560595984427, 19.416225271692667, 19.410416528615976, 19.403025170606504, 19.387404188240975, 19.37797667291035, 19.352779189071693, 19.34339615822546, 19.31628372748908, 19.306481668603254, 19.200575804631892, 19.173743561997394, 19.171195870935186, 19.161807532026558, 19.161446555929736, 19.15642626581945, 19.15296383468037]
+    sizes = []
+    for i, _ in enumerate(labels):
+        sizes.append(num_pairs.count(i+1))
+    
+    labels = ['286 and 303', '274 and 301', '285 and 289', '286 and 307', '300 and 301', 'Other speaker pairs']
+    sizes = [34,24,13,8,4,17]
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax1.set_title('False Positive Speaker Pairs')
+
     #plt.show()
-    plt.savefig('False_Positive_Scores.svg')
+    plt.savefig('False_Positive_Scores.pdf', bbox_inches='tight')
 
-    # fig, ax = plt.subplots()
+if(False):
+    plt.rcParams.update({'font.size': 16})
 
-    # colors = np.where(pairs==2, 'r', 'b')
+    x = np.linspace(-10, 10, 100)
+    f = 1 / (1 + np.exp(-x))
 
-    # ax.barh(y_pos, performance*-1, color=colors)
-    # ax.invert_yaxis()
-    # ax.set_xlabel('LLR Score')
-    # ax.set_title('False Positive Scores')
+    plt.axhline(color="grey")
+    plt.axvline(color="grey")
+    plt.plot(x, f, linewidth=2, label=r"$f(x) = \frac{1}{1 + e^{-x}}$")
+    plt.xlim(-10, 10)
+    plt.xlabel("x")
+    plt.ylabel('y')
+    plt.legend(fontsize=20)
+    plt.savefig('sigmoid.pdf', bbox_inches='tight')
+    plt.close()
 
-    # plt.show()
+    x = np.linspace(-10, 10, 100)
+    f = np.where(x>0, x, 0)
 
-    # fig, ax = plt.subplots()
+    plt.axhline(color="grey")
+    plt.axvline(color="grey")
+    plt.plot(x, f, linewidth=2, label=r"$f(x) = max(0,x)$")
+    plt.xlim(-10, 10)
+    plt.xlabel("x")
+    plt.ylabel('y')
+    plt.legend(fontsize=20)
+    plt.savefig('relu.pdf', bbox_inches='tight')
+    plt.close()
+    
+    x = np.linspace(-10, 10, 100)
+    f = np.where(x>0, x, 0.1*x)
 
-    # colors = np.where(pairs==3, 'r', 'b')
+    plt.axhline(color="grey")
+    plt.axvline(color="grey")
+    plt.plot(x, f, linewidth=2, label=r"$f(x) = max(\alpha x,x)$")
+    plt.xlim(-10, 10)
+    plt.xlabel("x")
+    plt.ylabel('y')
+    plt.legend(fontsize=20)
+    plt.savefig('leakyrelu.pdf', bbox_inches='tight')
+    plt.close()
 
-    # ax.barh(y_pos, performance*-1, color=colors)
-    # ax.invert_yaxis()
-    # ax.set_xlabel('LLR Score')
-    # ax.set_title('False Positive Scores')
+    print('done')
 
-    # plt.show()
+if(False):
+    plt.rcParams.update({'font.size': 14})
 
-    # fig, ax = plt.subplots()
+    x = np.linspace(-1, 30, 1000)
 
-    # colors = np.where(pairs==1, 'r', 'b')
-    # colors = np.where(pairs==2, 'r', colors)
-    # colors = np.where(pairs==3, 'r', colors)
+    a = 0.1 * np.sin(x*5)
+    b = 0.3 * np.sin(x*3)
+    c = 0.5 * np.sin(x*15)
 
-    # ax.barh(y_pos, performance*-1, color=colors)
-    # ax.invert_yaxis()
-    # ax.set_xlabel('LLR Score')
-    # ax.set_title('False Positive Scores')
+    d = a+b+c
+    e = a-0.5*c
 
-    # plt.show()
+    f = -25*np.log10(x*0.01 + 0.05) - 10 + d
+    g = 0.04*(x-20)**2 + 8 + e
 
-    Print('done')
+    plt.figure(figsize=(10,5))
+    plt.axhline(color="grey")
+    plt.axvline(color="grey")
+    plt.axvline(linewidth=2, x=19.8, color="green")
+    plt.plot(x, f, linewidth=2, label='train_loss')
+    plt.plot(x, g, linewidth=2, label='val_loss')
+    plt.xlim(-1, 30)
+    plt.xlabel("x")
+    plt.ylabel('y')
+    plt.legend(fontsize=16)
+    plt.savefig('overfitting.pdf', bbox_inches='tight')
+    plt.close()
+    print('done')
 
-t = np.linspace(-10, 10, 100)
-sig = 1 / (1 + np.exp(-t))
-
-plt.axhline(y=0, color="black", linestyle="--")
-plt.axhline(y=0.5, color="black", linestyle=":")
-plt.axhline(y=1.0, color="black", linestyle="--")
-plt.axvline(color="grey")
-plt.axline((0, 0.5), slope=0.25, color="black", linestyle=(0, (5, 5)))
-plt.plot(t, sig, linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
-plt.xlim(-10, 10)
-plt.xlabel("t")
-plt.legend(fontsize=14)
-plt.savefig('sigmoid.pdf')
